f4293ddc6b1f2773e1acb40e52198321bf7a2aee
unknown <beridzegigi19@gmail.com>
2025-02-15 10:08:25 -0800

    All

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,4 +1,4 @@
-import { ArrowDown, ArrowDownCircle, BookDown, Expand, MessagesSquare, MoveDown } from 'lucide-react'
+import { ArrowDown, ArrowDownCircle, BookDown, Expand, LogOut, MessagesSquare, MoveDown, X } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import Howitworks from './Howitworks';
 import Main from './Main';
@@ -7,11 +7,30 @@ import TranslatorComponent from './TranslatorComponent';
 export default function Header() {
 
   const [engopened , setengopened] = useState(false)
+  const [menuopened , setmenuopened] = useState(false)
 
 
   
   return (
    <div className='flex landingheader  items-center  p-[20px] justify-between' >
+
+    {menuopened == true  ? 
+    
+    <div className="openedmenu  p-[20px]">
+      <div className="menuline w-[100%] flex items-center justify-between">Menu  <button onClick={() => setmenuopened(false)} ><X className='text-red-500' ></X></button></div>
+<br /><br />
+      <div className="flex flex-col items-start justify-start gap-[10px]">
+      <a className='text-[24px] text-start w-[120px]  rounded-[5px]' href="/register">Dashboard</a>
+        <a className=' text-[24px] w-[120px] rounded-[5px]' href="/register">Sign Up</a>
+        <a  className='text-[24px]  w-[120px]  rounded-[5px]' href="/login">Sign In</a>
+      </div>
+
+    </div>
+
+    : null}
+
+     
+
     <div className="logo flex items-center gap-[15px]">
       <div className="logoframe bg-indigo-500 p-[10px]">
         <MessagesSquare className='w-[35px] h-[35px]'></MessagesSquare>
@@ -37,7 +56,7 @@ export default function Header() {
       <button className='p-[7px] bg-teal-500 text-white rounded-[3px]'>Get Started</button>
     </div>
     <div className="Menu hidden">
-      <button><img width={30} src="Menu.png" alt="" /></button>
+      <button onClick={() => setmenuopened(true)} ><img width={30} src="Menu.png" alt="" /></button>
     </div>
    </div>
   )
--- app/components/Howitworks.jsx
+++ app/components/Howitworks.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 export default function Howitworks() {
   return (
    <div className="howitworks relative">
-    <div className="lights"></div>
-    <div className="lights2"></div>
+
+
     <div className="howitworksframe flex flex-col items-center ">
         <h1 className="hoitworkstittle w-[100%] flex items-center justify-center text-[64px] mt-[25px] font-[600]">How it Works ?</h1>
         <h2 className='text-white text-center flex items-center justify-center mt-[25px] text-[24px] w-[50%]' >You enter the application, then you select the languages ​​you can speak, you also select the languages ​​you want to learn, then you enter the panel where there are other people with whom you can start talking and learning.</h2>
         <br />
         <div className="steps flex items-center justify-evenly w-[80%] gap-[25px]">
           <div className="woman1">
+          <div className="lights"></div>
             <div className="womandesc text-white">This Is Ana He is Speaking German Language from 3 years He Wants To Learn English And Practice</div>
           </div>
           <div className="woman2">
+          <div className="lights2"></div>
           <div className="womandesc text-white">This Is Mziuri He is Speaking English Language from 2 years He Wants To Learn German Language</div>
           </div>
 
